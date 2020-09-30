c58d6b19665160cc927c279498213a59d5199d46
Vaibhav Goyal <vaibhav@tagmango.com>
2020-09-30 10:45:43 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FaAngleRight, FaFileCode, FaJs, FaHtml5, FaCss3, FaInfoCircle, FaGitAlt} from "react-icons/fa";
+import {data} from "./structure"
 
-function App() {
+const Folder = ({ name, children }) => {
+  const [isExpand, setIsExpand] = useState(false);
+
+  const handleToggle = (e) => {
+    e.preventDefault();
+    setIsExpand(!isExpand);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FolderDiv>
+      <div className="folder-name" onClick={handleToggle}>
+        <FaAngleRight />
+        <Title>{name}</Title>
+      </div>
+      <ToggleFolder isExpand={isExpand}>{children}</ToggleFolder>
+    </FolderDiv>
+  );
+};
+
+const FILE_EXT_ENUM = {
+  js: <FaJs />,
+  html: <FaHtml5 />,
+  css: <FaCss3 />,
+  gitignore: <FaGitAlt />,
+  md: <FaInfoCircle />
+};
+
+const File = ({ name }) => {
+  let ext = name.split(".")[1];
+
+  return (
+    <FileDiv>
+      {FILE_EXT_ENUM[ext] || <FaFileCode />}
+      <Title>{name}</Title>
+    </FileDiv>
+  );
+};
+
+const RenderNodeRecursively = ({ data }) => {
+  return data.map((item) => {
+    
+    if (item.children.length === 0) {
+      return <File name={item.title} key={item.title}/>;
+    }
+    if (item.children.length > 0) {
+      return (
+        <Folder name={item.title} key={item.title}>
+          <RenderNodeRecursively data={item.children} key={item.children} />
+        </Folder>
+      );
+    }
+  });
+};
+
+export default function App() {
+  return (
+    <StyledNode>
+    <RenderNodeRecursively data={data} />
+    </StyledNode>
   );
 }
 
-export default App;
+
+const StyledNode = styled.div`
+  line-height: 1.5;
+`;
+
+const FolderDiv = styled.div`
+  padding-left: 20px;
+
+  .folder-name {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Title = styled.div`
+      margin-left: 5px;
+`;
+const FileDiv = styled.div`
+  display: flex;
+  padding-left: 25px;
+  align-items: center;
+`;
+const ToggleFolder = styled.div`
+  height: ${(p) => (p.isExpand ? "auto" : "0")};
+  overflow: hidden;
+`;
---
+++ src/structure.js
@@ -0,0 +1,41 @@
+export const data = [
+    {
+        title: 'src',
+        children: [
+            {
+                title: 'components',
+                children: [
+                  { title: "MainComponent.js", children:[] },
+                  { title: "sample.js", children:[] }
+                ]
+            },
+            {
+                title: 'redux',
+                children: [
+                     {title: "actions", children: []},
+                     {title: "reducers", children: []},
+                ]
+            }
+        ]
+    },
+    {
+        title: 'assets',
+        children: [
+            { title: 'images',children: [] },
+            { title: 'style.css',children: [] },
+            { title: "fonts", children: [] }
+        ]
+    },
+    {
+      title:"index.html", children: []
+    },
+    {
+        title:"structure.js", children: []
+    },
+    {
+        title:".gitignore", children: []
+    },
+    {
+        title:"README.md", children: []
+    }
+  ]
